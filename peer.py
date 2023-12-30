d7b9f83f16285985ecd180482f76a23ef6a73eda
George Welson <georgeezwelson@outlook.com>
2023-12-30 16:38:55 +0200

    threads bayza 2

--- peer.py
+++ peer.py
@@ -5,14 +5,46 @@ import time
 import logging
 import hashlib
 import string
-
-import db
+import select
 from db import DB
 import random
 import re
 import pwinput
-from peerVars import bcolors, registryPort
-from peerServer import PeerServer
+
+global chat_log
+chat_log = ''
+# Server side of peer
+
+global registryIP
+registryIP = ''
+global registryPort
+registryPort = 15600
+
+
+class bcolors:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    LIGHT_GRAY = '\033[37m'
+    DARK_GRAY = '\033[90m'
+    LIGHT_RED = '\033[91m'
+    LIGHT_GREEN = '\033[92m'
+    LIGHT_YELLOW = '\033[93m'
+    LIGHT_BLUE = '\033[94m'
+    LIGHT_MAGENTA = '\033[95m'
+    LIGHT_CYAN = '\033[96m'
+    WHITE = '\033[97m'
+
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    BLINK = '\033[5m'
+    REVERSE = '\033[7m'
+    CONCEAL = '\033[8m'
+    ENDC = '\033[0m'
 # Client side of peer
 class PeerClient(threading.Thread):
     global chat_log
@@ -344,7 +376,169 @@ class PeerClientRoom(threading.Thread):
 
 
 
+class PeerServer(threading.Thread):
+    global chat_log
 
+    # Peer server initialization
+    def __init__(self, username, peerServerPort):
+        threading.Thread.__init__(self)
+        # keeps the username of the peer
+        self.username = username
+        # tcp socket for peer server
+        self.tcpServerSocket = socket(AF_INET, SOCK_STREAM)
+        # port number of the peer server
+        self.peerServerPort = peerServerPort
+        # if 1, then user is already chatting with someone
+        # if 0, then user is not chatting with anyone
+        self.isChatRequested = 0
+        # keeps the socket for the peer that is connected to this peer
+        self.connectedPeerSocket = None
+        # keeps the ip of the peer that is connected to this peer's server
+        self.connectedPeerIP = None
+        # keeps the port number of the peer that is connected to this peer's server
+        self.connectedPeerPort = None
+        # online status of the peer
+        self.isOnline = True
+        # keeps the username of the peer that this peer is chatting with
+        self.chattingClientName = None
+
+        # TSL connection
+        # context = ssl.create_default_context()
+        # self.tcpClientSocket = context.wrap_socket(self.tcpClientSocket, server_hostname="192.168.1.7")
+
+    # main method of the peer server thread
+    def get_username(self):
+        return self.username
+
+    def run(self):
+
+        global chat_log
+        print("Peer server started...")
+
+        # gets the ip address of this peer
+        # first checks to get it for windows devices
+        # if the device that runs this application is not windows
+        # it checks to get it for macos devices
+
+        hostname = gethostname()
+        try:
+            self.peerServerHostname = gethostbyname(hostname)
+        except gaierror:
+            import netifaces as ni
+            self.peerServerHostname = ni.ifaddresses('en0')[ni.AF_INET][0]['addr']
+
+        # ip address of this peer
+        # self.peerServerHostname = 'localhost'
+        # socket initializations for the server of the peer
+        self.tcpServerSocket.bind((self.peerServerHostname, self.peerServerPort))
+        self.tcpServerSocket.listen(4)
+        # inputs sockets that should be listened
+        inputs = [self.tcpServerSocket]
+        # server listens as long as there is a socket to listen in the inputs list and the user is online
+        while inputs and self.isOnline:
+            # monitors for the incoming connections
+            try:
+                readable, writable, exceptional = select.select(inputs, [], [])
+                # If a server waits to be connected enters here
+                for s in readable:
+                    # if the socket that is receiving the connection is
+                    # the tcp socket of the peer's server, enters here
+                    if s is self.tcpServerSocket:
+                        # accepts the connection, and adds its connection socket to the inputs list
+                        # so that we can monitor that socket as well
+                        connected, addr = s.accept()
+                        connected.setblocking(0)
+                        inputs.append(connected)
+                        # if the user is not chatting, then the ip and the socket of
+                        # this peer is assigned to server variables
+                        if self.isChatRequested == 0:
+                            print(self.username + " is connected from " + str(addr))
+                            self.connectedPeerSocket = connected
+                            self.connectedPeerIP = addr[0]
+                    # if the socket that receives the data is the one that
+                    # is used to communicate with a connected peer, then enters here
+                    else:
+                        # message is received from connected peer
+                        messageReceived = s.recv(1024).decode()
+                        # print("MESSAGE RECEIVED HEREEEEEE " + messageReceived)
+                        # logs the received message
+                        logging.info("Received from " + str(self.connectedPeerIP) + " -> " + str(messageReceived))
+                        # if message is a request message it means that this is the receiver side peer server
+                        # so evaluate the chat request
+                        if len(messageReceived) > 11 and messageReceived[:12] == "CHAT-REQUEST":
+                            # text for proper input choices is printed however OK or REJECT is taken as input in main process of the peer
+                            # if the socket that we received the data belongs to the peer that we are chatting with,
+                            # enters here
+                            if s is self.connectedPeerSocket:
+                                # parses the message
+                                messageReceived = messageReceived.split()
+                                # gets the port of the peer that sends the chat request message
+                                self.connectedPeerPort = int(messageReceived[1])
+                                # gets the username of the peer sends the chat request message
+                                self.chattingClientName = messageReceived[2]
+                                # prints prompt for the incoming chat request
+                                # print("Incoming chat request from " + self.chattingClientName + " >> ")
+                                # print("Enter OK to accept, REJECT to reject : ")
+                                print(
+                                    bcolors.LIGHT_CYAN + self.chattingClientName + " has sent you a chat request!" + bcolors.ENDC)
+                                print(
+                                    bcolors.LIGHT_CYAN + "Enter OK to accept, or REJECT to reject the chat request: " + bcolors.ENDC)
+                                # makes isChatRequested = 1 which means that peer is chatting with someone
+                                self.isChatRequested = 1
+                            # if the socket that we received the data does not belong to the peer that we are chatting with
+                            # and if the user is already chatting with someone else(isChatRequested = 1), then enters here
+                            elif s is not self.connectedPeerSocket and self.isChatRequested == 1:
+                                # sends a busy message to the peer that sends a chat request when this peer is
+                                # already chatting with someone else
+                                # print('XDDDDD LMAOOOOOOO')
+                                message = "BUSY"
+                                s.send(message.encode())
+                                # remove the peer from the inputs list so that it will not monitor this socket
+                                inputs.remove(s)
+                        # if an OK message is received then ischatrequested is made 1 and then next messages will be shown to the peer of this server
+                        elif messageReceived == "OK":
+                            self.isChatRequested = 1
+                        # if an REJECT message is received then ischatrequested is made 0 so that it can receive any other chat requests
+                        elif messageReceived == "REJECT":
+                            self.isChatRequested = 0
+                            inputs.remove(s)
+                        # if a message is received, and if this is not a quit message ':q' and
+                        # if it is not an empty message, show this message to the user
+                        elif messageReceived[:2] != ":q" and len(messageReceived) != 0:
+                            chat_log = chat_log + bcolors.LIGHT_GREEN + self.chattingClientName + bcolors.ENDC + ": " + messageReceived + '\n'
+                            os.system('cls')
+                            print(chat_log)
+                            # print(self.username + ": ", end='')
+                            # print(self.chattingClientName + ": " + messageReceived)
+                            # print(self.username + ": ")
+                        # if the message received is a quit message ':q',
+                        # makes ischatrequested 1 to receive new incoming request messages
+                        # removes the socket of the connected peer from the inputs list
+                        elif messageReceived[:2] == ":q":
+                            self.isChatRequested = 0
+                            inputs.clear()
+                            inputs.append(self.tcpServerSocket)
+                            chat_log = ''
+                            # connected peer ended the chat
+                            if len(messageReceived) == 2:
+                                os.system('cls')
+                                print(bcolors.LIGHT_RED + "User you're chatting with ended the chat" + bcolors.ENDC)
+                                print(bcolors.LIGHT_RED + "Press enter to quit the chat: " + bcolors.ENDC)
+                        # if the message is an empty one, then it means that the
+                        # connected user suddenly ended the chat(an error occurred)
+                        elif len(messageReceived) == 0:
+                            self.isChatRequested = 0
+                            inputs.clear()
+                            inputs.append(self.tcpServerSocket)
+                            os.system('cls')
+                            print(bcolors.LIGHT_RED + "User you're chatting with ended the chat" + bcolors.ENDC)
+                            print(bcolors.LIGHT_RED + "Press enter to quit the chat: " + bcolors.ENDC)
+                            chat_log = ''
+            # handles the exceptions, and logs them
+            except OSError as oErr:
+                logging.error("OSError: {0}".format(oErr))
+            except ValueError as vErr:
+                logging.error("ValueError: {0}".format(vErr))
 
 
 
--- registryVars.py
+++
@@ -1,9 +0,0 @@
-# onlinePeers list for online account
-onlinePeers = {}
-# accounts list for accounts
-accounts = {}
-# tcpThreads list for online client's thread
-tcpThreads = {}
-
-port = 15600
-portUDP = 15500
