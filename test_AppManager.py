fca87ba18c4b8c062c5658cc9d408385bd345322
George Welson <georgeezwelson@outlook.com>
2024-01-03 01:34:20 +0200

    Testing AppManager

--- test_AppManager.py
+++ test_AppManager.py
@@ -1,9 +1,32 @@
 import unittest
 from AppManager import *
 
+
 class TestAppManager(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def setUp(self):
+        self.appManager = AppManager()
+
+    def test_password_length(self):
+        self.assertFalse(self.appManager.check_password_policy("short"))  # Test for password too short
+
+    def test_password_digit(self):
+        self.assertFalse(self.appManager.check_password_policy("no_digit"))  # Test for no digits in password
+
+    def test_password_special_character(self):
+        self.assertFalse(self.appManager.check_password_policy("no_special"))  # Test for no special characters
+
+    def test_password_spaces(self):
+        self.assertFalse(self.appManager.check_password_policy("has space"))  # Test for password containing spaces
+
+    def test_valid_password(self):
+        self.assertTrue(self.appManager.check_password_policy("GoodPass1!"))  # Test for a valid password
+    def test_hash_password(self):
+        password = "my_password"
+        hashed_password = self.appManager.hash_password(password)
+
+        expected_hash = hashlib.sha256(password.encode()).hexdigest()
+
+        self.assertEqual(hashed_password, expected_hash)
 
 
 if __name__ == '__main__':
